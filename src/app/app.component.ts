6507dc6cccfd18985f67bcbebd20e15324efbb5f
jwright7041 <jmwright120@gmail.com>
2021-06-13 16:25:19 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+export class AppComponent implements OnInit {
+  title = 'Todos';
+  todoList: any [] = [];
+  todoTitle: string;
+
+  ngOnInit(): void {
+    this.todoList = [
+      { title: 'Install Angular CLI', isDone: false }
+    ];
+    this.todoTitle = '';
+  }
+
+  deleteTodo(todo:any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
+
+  addTodo():void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false
+    });
+    
+    this.todoTitle = '';
+  }
 }
